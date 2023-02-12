aefb06774bd06faff368fbec872aa3802815fdf5
Jeff Fessler <fessler@umich.edu>
2023-02-12 07:09:28 -0500

    inc/urls

---
+++ docs/inc/urls.jl
@@ -0,0 +1,14 @@
+
+#=
+This page was generated using a single Julia file:
+[`xxx.jl`](@__REPO_ROOT_URL__/xxx.jl).
+
+You can access the source code
+for such Julia documentation
+using the "Edit on GitHub" link in the top right.
+You can view the corresponding notebook in
+[nbviewer](https://nbviewer.org/) here:
+[`xxx.ipynb`](@__NBVIEWER_ROOT_URL__/xxx.ipynb),
+or open it in [binder](https://mybinder.org/) here:
+[`xxx.ipynb`](@__BINDER_ROOT_URL__/xxx.ipynb).
+=#
