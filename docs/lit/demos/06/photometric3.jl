2222f54c5facbc1829029e94e8af63c445324dba
Jeff Fessler <fessler@umich.edu>
2023-03-12 23:31:45 -0400

    procrustes

--- docs/lit/demos/06/photometric3.jl
+++ docs/lit/demos/06/photometric3.jl
@@ -20,7 +20,7 @@ Change `false` to `true` in the following code block
 if you are using any of the following packages for the first time.
 =#
 
-if false
+if false # todo
     import Pkg
     Pkg.add([
         "InteractiveUtils"
@@ -57,19 +57,21 @@ using Random: seed!
 Load ground truth surface normal vectors of "bunny" data used in
 [2012 CVPR paper by Ikehata et al.](https://doi.org/10.1109/CVPR.2012.6247691).
 =#
-if !@isdefined(gt_normal)
+if !@isdefined(gt_normal_bunny)
     url = "https://github.com/yasumat/RobustPhotometricStereo/raw/master/data/bunny/gt_normal.npy"
     tmp = npzread(download(url))
     i1 = 22:256-25
     i2 = 28:217
     tmp = permutedims(tmp, [2, 1, 3]) # "transpose"
-    tmp = tmp[i1, i2, :] # crop
+    gt_normal_bunny = tmp[i1, i2, :] # crop
+end
 # todo cut:
 #   tmp = tmp[:, :, [2, 1, 3]] # transpose?
 #   tmp[:,:,3] .*= -1 # sign flip
-    gt_normal = tmp
-    nx, ny = size(gt_normal)[1:2]
+if !@isdefined(gt_normal)
 end
+    gt_normal = gt_normal_bunny
+    nx, ny = size(gt_normal)[1:2]
 
 function hemi_normal(x, y ; # surface normal of a hemi-ellipsoid
     xh = 70f0,
@@ -98,7 +100,7 @@ function hemi_normal(x, y ; # surface normal of a hemi-ellipsoid
     return [0, 0, 0]
 end
 
-if true
+if false
     x = (1:nx) .- (nx+1)/2
     y = (1:ny) .- (ny+1)/2
     tmp = hemi_normal.(x, y')
@@ -122,9 +124,9 @@ function random_light(;
     θ = rand() * 2π,
 )
 #   r = rand()^0.5 * rmax
-    r = 0.5
-    x = r * cos(θ) + 0.1
-    y = r * sin(θ) + 0.1
+    r = rmax
+    x = r * cos(θ) + 0.2
+    y = r * sin(θ) + 0.2
     z = sqrt(1 - x^2 - y^2)
     return [x, y, abs(z)]
 end 
@@ -134,8 +136,9 @@ end
     seed!(1)
     nlight = 20 # number of lighting directions
 #   Ltrue = [random_light() for _ in 1:nlight]
-    Ltrue = [random_light(; θ=il/nlight*1.5π) for il in 1:nlight]
+    Ltrue = [random_light(; θ=il/nlight*1.7π, rmax=0.5-0.1*il/nlight) for il in 1:nlight]
     Ltrue = hcat(Ltrue...)'
+@show extrema(Ltrue[:,3])
 
 tmp = range(0, 2π, 361)
 plot(cos.(tmp), sin.(tmp), aspect_ratio=1,
@@ -156,6 +159,9 @@ With self-shadow effects
 [Barsky & Petrou, 2003, T-PAMI](https://doi.org/10.1109/TPAMI.2003.1233898),
 there are 3 dominant singular values
 and additional small but non-negligible values.
+
+[Wu et al., ACCV, 2011](https://doi.org/10.1007/978-3-642-19318-7_55)
+describe the `max(⋅,0)` as "attached shadows."
 =#
 
 if !@isdefined(images)
@@ -181,6 +187,7 @@ end
     images_good = reshape(images, :, nlight)[vec(good),:]
     svdval_good = svdvals(images_good)
     scatter!(ps, svdval_good, label="Good pixels", color=:green)
+# todo: tail svdvals
 ps
 
 #
@@ -201,6 +208,7 @@ unlike the
 [original least-squares approach of Woodham, 1980](https://doi.org/10.1117/12.7972479).
 =#
 
+#=
 if !@isdefined(images) && false
     url0 = "https://github.com/yasumat/RobustPhotometricStereo/raw/master/data/bunny/bunny_lambert/image000.npy"
 
@@ -229,6 +237,7 @@ if !@isdefined(images) && false
 ## https://raw.githubusercontent.com/yasumat/RobustPhotometricStereo/master/data/bunny/mask.png
 end
 pd = jim(images; title="Images for different lighting directions")
+=#
 
 #src savefig("photometric3_data.pdf")
 
@@ -263,29 +272,64 @@ Estimate the lighting directions
 using _only_ the pixels with no
 =#
 
-# The following permutation / sign-flip matrix was found empirically:
-P = [0 0 -1; 0 1 0; 1 0 0]
-
-light = svd(images_good).V[:,1:3]
-light ./= sqrt.(sum(abs2, light, dims = 2)) # normalize to unit norm
-light *= P # todo
-
-tmp = deepcopy(pl_gt)
-pl = scatter!(tmp, light[:,1], light[:,2], label = "Estimated")
+tmp = svd(images_good)
+light0 = tmp.V[:,1:3] # * Diagonal(sqrt.(tmp.S[1:3]))
 
 plot(Ltrue)
-scatter!(light)
-
+scatter!(light0)
 
 #=
+Apply method of
+[Hayakawa, JOSA, 1994](https://doi.org/10.1364/JOSAA.11.003079)
+to resolve non-uniqueness issue,
+under the simplifying assumption
+(satisfied here)
+that the light intensity is the same
+for all lighting directions.
+=#
 
-# The following permutation / sign-flip matrix was found empirically:
-P = [0 0 -1; 0 1 0; 1 0 0]
+Abig = reduce(vcat, map(r -> kron(r', r'), eachrow(light0)))
+# Abig = Abig[:,[1, 2, 3, 5, 6, 8]] # symmetry of B
+tmp = Abig \ ones(nlight)
+# tmp = tmp[[1, 2, 3, 2, 4, 5, 3, 5, 6]]
+B = reshape(tmp, 3, 3) # B = A'A
+@assert B ≈ B'
+tmp = svd(B)
+@assert tmp.U ≈ tmp.V # (symmetry)
+#A = Diagonal(sqrt.(tmp.S)) * tmp.V'
+A = sqrt(B)
+@assert A'A ≈ B
+light1 = (A * light0')'
+@assert collect(extrema(sum(abs2, light1, dims=2))) ≈ [1,1]
+
+# use Procrustes to align with true lighting
+tmp = Ltrue' * light1
+tmp = svd(tmp)
+tmp = tmp.U * tmp.Vt
+light1 = light1 * tmp'
+
+color = [:red :green :blue]
+plot(Ltrue; color)
+scatter!(light1; color)
+
 
 # Examine estimated lighting directions
 # from (permuted) first three right singular vectors.
-L = V[:,1:3] # * P # notice permutation
-L ./= sqrt.(sum(abs2, L, dims = 2)) # normalize to unit norm
+
+#L = V[:,1:3] # * P # notice permutation
+#L ./= sqrt.(sum(abs2, L, dims = 2)) # normalize to unit norm
+
+tmp = deepcopy(pl_gt)
+pl = scatter!(tmp, light1[:,1], light1[:,2], marker = :x, label = "Estimated")
+
+#
+prompt()
+
+#= todo
+
+# The following permutation / sign-flip matrix was found empirically:
+P = [0 0 -1; 0 1 0; -1 0 0]
+light1 = light1 * P
 
 pl = scatter(L[:,1], L[:,2], L[:,3],
    xaxis = (L"x", (-1, 1), (-1:1)*0.8),
@@ -293,10 +337,6 @@ pl = scatter(L[:,1], L[:,2], L[:,3],
    zaxis = (L"z", (0.2, 1.0), 0.2:0.1:1.0),
    title = "Estimated lighting directions",
 )
-
-#
-prompt()
-
 
 #=
 Next we examine the estimated surface normals.
