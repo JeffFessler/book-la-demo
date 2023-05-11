a1334ad587ce11b85064e16f056bbe6c9cd4c23b
Jeff Fessler <fessler@umich.edu>
2023-05-10 21:44:46 -0400

    shiny

--- docs/src/assets/logo.jl
+++ docs/src/assets/logo.jl
@@ -8,19 +8,32 @@ seed!(0)
 const julia_purple  = (0.584, 0.345, 0.698)
 const julia_green   = (0.22, 0.596, 0.149)
 const julia_red     = (0.796, 0.235, 0.2)
-colors = [ julia_purple, julia_green, julia_red, ] # from Luxor.jl
-colors = [RGB{Float32}(c...) for c in colors]
-black = RGB{Float32}(0, 0, 0)
+color1 = [julia_purple, julia_green, julia_red] # from Luxor.jl
+colors = [RGB{Float32}(c...) for c in color1]
+# black = RGB{Float32}(0, 0, 0)
 
-M,N = 20,16
+N = 50 # box size
+function chiclet(c; pad::Float32=1.2f0)
+    u = range(-1, 1, N) * pad
+    p = 8
+    mask = @. (u^p .+ u'^p)^(1/p) < 0.99
+    x = range(-1, 1, N) * 0.09
+    tmp = [RGB{Float32}((c .- (1,1,1) .* x)...) for x in x]
+    tmp = tmp * ones(N)' .* mask
+    return tmp
+end
+boxes = chiclet.(color1)
+
+M,N = 20,16 # grid size
 pcolor = 0.8 # probability of a color
 
-logo = fill(black, M, N)
+inds = zeros(Int, M, N)
 ii = randperm(M*N)[1:Int(pcolor*M*N)]
-tmp = rand(1:3, length(ii)) # too clustered, despite random?  need poisson disc?
-logo[ii] = colors[tmp]
+inds[ii] = rand(1:3, length(ii)) # too clustered?  need poisson disc?
 
-z = zeros(3)
-z = [z; ones(9); z]
-logo = kron(logo, z*z')
-save("logo.png", logo)
+logo = kron(inds .== 1, boxes[1])
+for i in 2:3
+    logo .+= kron(inds .== i, boxes[i])
+end
+# save("logo.png", logo)
+# run(`xv logo.png`)
