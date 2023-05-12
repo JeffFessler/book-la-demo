4b22e1ed467493ae10b15f07dec4c4f73d6061bd
Jeff Fessler <JeffFessler@users.noreply.github.com>
2023-05-11 23:05:19 -0400

    mds with bias (#20)

--- docs/lit/demos/06/source-local.jl
+++ docs/lit/demos/06/source-local.jl
@@ -37,7 +37,7 @@ using MIRTjim: jim, prompt
 using Plots: default, scatter, savefig
 using Random: seed!
 using Statistics: mean
-default(label="", markerstrokecolor=:auto,
+default(label="", markerstrokecolor=:auto, widen=true,
     guidefontsize=14, tickfontsize=12, legendfontsize=14)
 
 
@@ -79,17 +79,18 @@ pd = jim(D, L"D", color=:cividis, xlabel=L"j", ylabel=L"i")
 
 # Compute Gram matrix by de-meaning squared distance matrix
 S = D.^2 # squared distances
-G = S .- mean(S,dims=1) # trick: use "broadcasting" feature of julia
-G = G .- mean(G,dims=2) # now we have de-meaned the columns and rows of S
-G = -1/2 * G
-# still cannot determine visually the point locations:
+G = S .- mean(S, dims=1) # trick: use "broadcasting" feature of julia
+G = G .- mean(G, dims=2) # now we have de-meaned the columns and rows of S
+G = -1/2 * G;
+
+# We still cannot determine visually the point locations:
 pg = jim(G, L"G", color=:cividis, xlabel=L"j", ylabel=L"i")
 
 #src savefig(pg, "06_source_local1_g.pdf")
 
 # Examine singular values
 (_, σ, V) = svd(G) # svd returns singular values in descending order
-ps = scatter(σ, label="singular values", widen=true,
+ps = scatter(σ, label="singular values (noiseless case)",
     xlabel=L"k", ylabel=L"σ_k") # two nonzero (d=2)
 
 #
@@ -103,7 +104,7 @@ Ch = Diagonal(sqrt.(σ[1:2])) * V[:,1:2]' # here is the key step
 
 # ### Plot estimated source locations
 pc = scatter(Ch[1,:], -Ch[2,:], xtick=-4:4, ytick=-3:3, aspect_ratio=1,
- title="Location estimates", widen=true)
+ title="Location estimates (noiseless case)")
 
 #
 prompt()
@@ -113,23 +114,48 @@ prompt()
 seed!(0)
 Dn = D + 0.3 * randn(size(D))
 Sn = Dn.^2
-Gn = Sn .- mean(Sn,dims=1)
-Gn = Gn .- mean(Gn,dims=2) # de-meaned
+Gn = Sn .- mean(Sn, dims=1)
+Gn = Gn .- mean(Gn, dims=2) # de-meaned
 Gn = -1/2 * Gn
-pgn = jim(Gn, "G noisy", color=:cividis) # still cannot determine visually the point locations
+pgn = jim(Gn, "G noisy", color=:cividis)
 
 
 # Singular values
 (_, sn, Vn) = svd(Gn)
-psn = scatter(abs.(sn), label="singular values") # two >> 0
+psn = scatter(sn, label="singular values (noisy case)") # σ₂ ≫ σ₃
 
 #
 prompt()
 
 # ### Plot estimated source locations from noisy distance measurements
-Cn = Diagonal(sqrt.(sn[1:2])) * Vn[:,1:2]' # here is the key step
+Cn = Diagonal(sqrt.(sn[1:2])) * Vn[:,1:2]'
 pcn = scatter(Cn[1,:], -Cn[2,:], xtick=-4:4, ytick=-3:3, aspect_ratio=1,
- title="Location estimates", widen=true)
+ title="Location estimates (noisy case)")
+
+#
+prompt()
+
+
+# ## Constant bias case
+seed!(0)
+Db = D .+ 0.3 * maximum(D) # fairly large bias
+Sb = Db.^2
+Gb = Sb .- mean(Sb, dims=1)
+Gb = Gb .- mean(Gb, dims=2) # de-meaned
+Gb = -1/2 * Gb
+pgb = jim(Gb, "G biased", color=:cividis)
+
+# Singular values
+(_, sb, Vb) = svd(Gb)
+psb = scatter(sb, label="singular values (biased case)") # σ₂ ≫ σ₃
+
+#
+prompt()
+
+# ### Plot estimated source locations from biased distance measurements
+Cb = Diagonal(sqrt.(sb[1:2])) * Vb[:,1:2]'
+pcb = scatter(Cb[1,:], -Cb[2,:], xtick=-4:4, ytick=-3:3, aspect_ratio=1,
+ title="Location estimates (biased case)")
 
 #
 prompt()
@@ -139,7 +165,7 @@ prompt()
 G = [-2 1 1; 1 -2 1; 1 1 -2] / (-6.)
 (~, σ, V) = svd(G)
 Ch = Diagonal(sqrt.(σ[1:2])) * V[:,1:2]'
-scatter(Ch[1,:], Ch[2,:], aspect_ratio=1, title="Location estimates", widen=true)
+scatter(Ch[1,:], Ch[2,:], aspect_ratio=1, title="Location estimates")
 
 #
 prompt()
