028923576f77d8b95932fa23b2808c7a198b1184
Jeff Fessler <fessler@umich.edu>
2023-11-08 21:43:25 -0500

    plots

--- docs/lit/demos/09/logistic1.jl
+++ docs/lit/demos/09/logistic1.jl
@@ -25,7 +25,7 @@ if false
         "MIRTjim"
         "Plots"
         "Random"
-#       "StatsBase"
+        "StatsBase"
     ])
 end
 
@@ -34,15 +34,12 @@ end
 # Run `Pkg.add()` in the preceding code block first, if needed.
 
 using InteractiveUtils: versioninfo
-using LaTeXStrings # nice plot labels
+using LaTeXStrings
 using LinearAlgebra: dot, eigvals
-using MIRTjim: jim, prompt
-#todo using MLDatasets: MNIST
+using MIRTjim: prompt
 using Plots: default, gui, savefig
 using Plots: plot, plot!, scatter, scatter!
-#using Plots: RGB, cgrad
-#using Plots.PlotMeasures: px
-using Random: seed! #, randperm
+using Random: seed!
 using StatsBase: mean
 default(); default(markersize=6, linewidth=2, markerstrokecolor=:auto, label="",
  tickfontsize=12, labelfontsize=18, legendfontsize=18, titlefontsize=18)
@@ -50,7 +47,7 @@ default(); default(markersize=6, linewidth=2, markerstrokecolor=:auto, label="",
 # The following line is helpful when running this file as a script;
 # this way it will prompt user to hit a key after each figure is displayed.
 
-#todo isinteractive() ? jim(:prompt, true) : prompt(:draw);
+isinteractive() ? prompt(:prompt) : prompt(:draw)
 
 
 #=
@@ -94,15 +91,28 @@ if !@isdefined(ps)
     scatter!(v1[1,:], v1[2,:], color=:blue, marker=:square, alpha=0.7)
     ## savefig(ps, "demo_fgm1_ogm1_s0.pdf")
 end
-ps
+plot(ps)
+
+#
 prompt()
+
 
 #=
 # Cost function
-todo: equation
-Lipschitz constant
+
+Logistic regression with Tikhonov regularization:
+```math
+f(x) = 1_M' h.(A x) + β/2 ‖ x ‖_2^2
+```
+where
+``h(z) = log(1 + e^{-z})``
+is the logistic loss function.
+
+Its gradient is
+``∇ f(x) = A' \dot{h}.(A x) + β x``,
+and its Lipschitz constant
+is ``‖A‖_2^2 / 4 + β``.
 =#
-# Lipschitz constant
 if !@isdefined(kost)
     pot = (t) -> log(1 + exp(-t)) # logistic
     dpot = (t) -> -1 / (exp(t) + 1)
@@ -112,72 +122,68 @@ if !@isdefined(kost)
     pLip = maximum(tmp) / 4 # 1/4 comes from logistic curvature
 
     reg = 2^0
-    Lip = pLip + reg
+    Lip = pLip + reg # Lipschitz constant
 
     A = yy .* vv'
-    gfun = x -> A' * dpot.(A * x) + reg * x
-    tmp = gfun(x0)
-    @show size(tmp)
+    gfun = x -> A' * dpot.(A * x) + reg * x # gradient
+    if false
+        tmp = gfun(x0)
+        @show size(tmp)
+    end
 
-#   kost = x -> pot.(x' * vv * yy)' + reg/2 * sum(abs2, x, dims=1)
-    kost = x -> ones(M)' * pot.(A * x) .+ reg/2 * sum(abs2, x, dims=1)
-# todo    kost = x -> sum(pot, A * x, dims=1) .+ reg/2 * sum(abs2, x, dims=1)
-end
+    kost = x -> sum(pot, A * x, dims=1) .+ reg/2 * sum(abs2, x, dims=1)
+end;
+
 
 #=
 # GD
 Iterate GD
 =#
-
 tol = 1e-6
 tol_gd = tol
 tol_n1 = tol
 tol_o1 = tol
-#todo   do_restart = false
-do_restart = true
 
-if !@isdefined(xgs)
-    niter_gd = 300
+function gd(x0, gfun::Function, niter::Int)
     xg = copy(x0)
     xgs = copy(xg)
-    for n in 1:niter_gd
+    for n in 1:niter
         xold = xg
-        global xg -= 1/Lip * gfun(xg)
+        xg -= 1/Lip * gfun(xg)
         if false && (norm(xg - xold, Inf) / norm(x0, Inf) < tol_gd)
             @show n
             break
         end
-        if any(isnan, xg)
-            throw("nan")
-        end
-        # projected GD ??
-#       xg = xg / norm(xg); # decision boundary not affected by norm!?
-        global xgs = [xgs xg] # archive
+        any(isnan, xg) && throw("nan")
+        ## projected GD ??
+        ## xg = xg / norm(xg) # decision boundary unaffected by norm!
+        xgs = [xgs xg] # archive
     end
+    return xgs
 end
 
-if false
-    v2p = @. (-xg[end] - xg[1] * v1p) / xg[2]
-    psg = deepcopy(ps)
-    plot!(psg, v1p, v2p, color = :magenta, label="final")
-
-    plot(xgs', title = "GD") # check convergence
+if !@isdefined(xgs)
+    niter_gd = 300
+    xgs = gd(x0, gfun, niter_gd)
+    pgs = plot(xgs', xlabel="Iteration", title = "GD")
 end
+plot(pgs)
 
+#
+prompt()
 
 #=
 # Nesterov FGM
 =#
+do_restart = true;
 
-if !@isdefined(xns)
+function fgm(x0, grad, Lip::Real, niter::Int)
     re_nest = Int[]
-    niter_n1 = 300
-    niter_n1 = 500 # nex
     told = 1
     x = copy(x0)
     zold = copy(x0)
     xns = copy(x)
-    for n in 1:niter_n1
+    for n in 1:niter
         xold = copy(x)
         grad = gfun(x)
         znew = x - 1/Lip * grad
@@ -185,19 +191,15 @@ if !@isdefined(xns)
 
         tnew = 1/2 * (1 + sqrt(1 + 4 * told^2))
 
-        global x = znew + (told - 1) / tnew * (znew - zold)
-        global zold = copy(znew)
-        global told = tnew
+        x = znew + (told - 1) / tnew * (znew - zold)
+        zold = copy(znew)
+        told = tnew
         if false && (norm(x - xold, Inf) / norm(x0, Inf) < tol_n1)
             @show n
             break
         end
-        if any(isnan, x)
-            throw("nan")
-        end
+        any(isnan, x) && throw("nan")
 
-    #    if do_restart && (dot(grad, x - xold) > 0) # jf old version
-    #    if do_restart && (dot(grad, x - znew) > 0) # dk new version
         if do_restart && (dot(grad, zdiff) > 0) # dk new version
             told = 1
             x = copy(znew) # check
@@ -205,31 +207,39 @@ if !@isdefined(xns)
             @show "nest. restart", n
             push!(re_nest, n)
         end
-        global xns = [xns x] # archive
+        xns = [xns x] # archive
     end
-    xn = xns[:,end]
+    return xns, re_nest
 end
 
-## plot(xns', title = "FGM")
+if !@isdefined(xns)
+    niter_n1 = 300
+    niter_n1 = 500 # nex
+    xns, re_nest = fgm(x0, gfun, Lip, niter_n1)
+    pns = plot(xns', xlabel = "Iteration", title = "FGM")
+end
+plot(pns)
+
+#
+prompt()
+
 
 #=
 ## OGM
-todo
+Optimized gradient method
 =#
 function ogm(
     x0,
     gfun,
     Lip::Real,
-    ;
-#   niter_o1 = 300,
-    niter_o1 = 500, # nex
+    niter::Int,
 )
     re_ogm1 = Int[]
     told = 1
     x = copy(x0)
     zold = copy(x0)
     x1s = copy(x)
-    for n in 1:niter_o1
+    for n in 1:niter
         xold = x
         grad = gfun(x)
         znew = x - 1/Lip * grad
@@ -246,80 +256,89 @@ function ogm(
         end
         any(isnan, x) && throw("nan")
 
-    ##   if do_restart && (dot(grad, x - xold) > 0) # jf old version
-    ##   if do_restart && (dot(grad, x - znew) > 0) # dk new version
         if do_restart && (dot(grad, zdiff) > 0) # dk new version
             push!(re_ogm1, n)
             told = 1
             x = znew # check
             zold = x # dk fixed from x0
-            @info("ogm1 restart $n")
+            @info "ogm1 restart $n"
         end
         x1s = [x1s x] # archive
     end
     return x1s, re_ogm1
 end
 
+
 if !@isdefined(x1s)
-    x1s, re_ogm1 = ogm(x0, gfun, Lip)
-    ## plot(x1s')
+    niter_o1 = 300
+    niter_o1 = 500 # nex
+    x1s, re_ogm1 = ogm(x0, gfun, Lip, niter_o1)
+    po1 = plot(x1s', xlabel = "Iteration", title = "OGM")
 end
-
-prompt()
-
-xh_tmp = [xgs[:,end] xns[:,end] x1s[:,end]]
-xh = vec(mean(xh_tmp[:,2:3], dims=2)) # GD too slow to include
-
-# plot cost
-plot(xaxis=("iteration", (0,10)), )
-plot!(0:niter_gd, vec(kost(xgs)) .- kost(xh), label="GD")
-plot!(0:niter_n1, vec(kost(xns)) .- kost(xh), label="FGM")
-plot!(0:niter_o1, vec(kost(x1s)) .- kost(xh), label="OGM1")
-
-
-# Plot decision boundaries
-if true
-    psn = deepcopy(ps)
-    v2p = @. (-xh[end] - xh[1] * v1p) / xh[2]
-    plot!(psn, v1p, v2p, color = :magenta, label="final")
-## savefig(psn, "demo-fgm1-fgm1a.pdf")
-end
-prompt()
-
-
-#=
-# plot iterate convergence
-=#
-
-plot(
- xaxis = ("Iteration", (0, 40+10*nex), 0:20:80),
-#src yaxis = ("‖ \\mathbf{x}_k - \\mathbf{x}_* ‖", (0, 8), [-1, 0, 8]),
- yaxis = (L"\log_{10}(‖ x_k - x_* ‖)", (-3, 1), -3:1),
-#legend = :bottomleft,
-)
-
-efun = (x) -> log10.(vec(sqrt.(sum(abs2, x .- xh, dims=1))))
-ifun = (x) -> 0:(size(x,2)-1)
-
-plot!(ifun(xgs), efun(xgs), color=:green, label="GD")
-plot!(ifun(xns), efun(xns), color=:blue, label="Nesterov FGM (restart)")
-plot!(ifun(x1s), efun(x1s), color=:red, label="OGM1 (restart)")
-if do_restart
-    scatter!(re_nest, efun(xns[:, re_nest .+ 1]), color=:blue)
-    scatter!(re_ogm1, efun(x1s[:, re_ogm1 .+ 1]), color=:red)
-end
-
-#   if ~do_restart
-#       ytick([0 8])
-#   end
-#   if do_restart
-    #    ir_savefig cw demo_fgm1_ogm1c # restart
-#   else
-#       legend('GD', 'Nesterov FGM', 'OGM1') # no restart
-    #    ir_savefig cw demo_fgm1_ogm1b # no restart
-#   end
+plot(po1)
 
 #
 prompt()
 
-#todo include("../../../inc/reproduce.jl")
+
+# impartial version of x ͚
+xh_tmp = [xgs[:,end] xns[:,end] x1s[:,end]]
+xh = vec(mean(xh_tmp[:,2:3], dims=2)); # GD too slow to include
+
+# plot cost
+extra = do_restart ? " (restart)" : ""
+pc = plot(xaxis=("iteration", (0,10)), yaxis=("Cost function",))
+plot!(0:niter_gd, vec(kost(xgs)) .- kost(xh), label = "GD" * extra)
+plot!(0:niter_n1, vec(kost(xns)) .- kost(xh), label = "FGM" * extra)
+plot!(0:niter_o1, vec(kost(x1s)) .- kost(xh), label = "OGM1" * extra)
+
+#
+prompt()
+
+
+# Plot decision boundaries
+if true
+    psh = deepcopy(ps)
+    v2p = @. (-xh[end] - xh[1] * v1p) / xh[2]
+    plot!(psh, v1p, v2p, color = :magenta, label="final")
+## savefig(psh, "demo-fgm1-fgm1a.pdf")
+end
+plot(psh)
+
+#
+prompt()
+
+
+#=
+# Plot iterate convergence
+=#
+
+efun1 = (x) -> vec(sqrt.(sum(abs2, x .- xh, dims=1)))
+efun = (x) -> do_restart ? log10.(efun1(x)) : efun1(x)
+ifun = (x) -> 0:(size(x,2)-1);
+
+pic = plot(
+ xaxis = ("Iteration", (0, 40+10*nex), 0:20:80),
+ yaxis = do_restart ?
+  (L"\log_{10}(‖ \mathbf{x}_k - \mathbf{x}_* ‖)", (-3, 1), -3:1) :
+  (L"‖ \mathbf{x}_k - \mathbf{x}_* ‖", (0, 8), [-1, 0, 8]),
+ legend = :topright,
+)
+plot!(ifun(xgs), efun(xgs), color=:green, label = "GD")
+plot!(ifun(xns), efun(xns), color=:blue, label = "Nesterov FGM" * extra)
+plot!(ifun(x1s), efun(x1s), color=:red, label = "OGM1" * extra)
+if do_restart
+    scatter!(re_nest, efun(xns[:, re_nest .+ 1]), color=:blue)
+    scatter!(re_ogm1, efun(x1s[:, re_ogm1 .+ 1]), color=:red)
+end
+plot(pic)
+
+#
+prompt()
+
+## savefig demo_fgm1_ogm1c # restart
+## savefig demo_fgm1_ogm1b # no restart
+
+## todo: compare with LBFGS
+
+include("../../../inc/reproduce.jl")
